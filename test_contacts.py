d9588531029c96fcfe345961e9b0ab5bb15ce86c
Mochualex4 <Mochualex4@gmail.com>
2018-03-01 08:30:03 +0300

    Create Contacts  object test case
    - The test for adding new contacts should pass

--- test_contacts.py
+++ test_contacts.py
@@ -7,6 +7,7 @@ from contacts import Contacts
 class ContactsTestCase(unittest.TestCase):
     """ Create Contacts object test case"""
     def test_add_contacts(self):
+        """ Test for adding new contacts """
         contacts = Contacts()
         response = contacts.add("alex", "0708913841")
         self.assertEqual(response, "Successfully added contacts" )
